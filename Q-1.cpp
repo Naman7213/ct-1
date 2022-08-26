54a99f005c74a0387c96721e2c4aedc5a82abb87
Naman <namanv08@gmail.com>
2022-08-26 13:36:42 +0530

    Question 1 Completed

--- Q-1.cpp
+++ Q-1.cpp
@@ -3,10 +3,33 @@
 //04
 //Q-1
 
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main(){
+	int n;
+	cin>>n;
+	int arr[n];
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+	}
+	int mI=n;
+	unordered_set<int> s;
+	for(int i=n-1;i>=0;i--){
+		if(s.find(arr[i])!=s.end()){
+			mI=i;
+		}
+		else{
+			s.insert(arr[i]);
+		}
+	}
+	if(mI==n){
+		cout<<"Invalid Input";
+	}
+	else{
+		cout<<"The minimum index of repeating element is "<<mI;	
+	}
+	
 	
 	return 0;
 }
